41340c1cf21184fbfb86b4f3d774f45a856aa0a7
Mason Capps <masoncapps42119@gmail.com>
2023-07-09 14:22:14 -0500

    Tests ignore file

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
@@ -6,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+mongoose.connect('mongodb+srv://masoncapps:Mc1013986@ratingprofilesapi.qerbcar.mongodb.net/Rating-Profiles-API?retryWrites=true&w=majority').then(() => {
+  console.log('connected to MongoDB')
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+}).catch(() => {
+  console.log
 })
