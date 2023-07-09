02e311c639fd4faaa1ba5348a2cc87e2edc0e555
Mason Capps <masoncapps42119@gmail.com>
2023-07-09 14:36:58 -0500

    Credentials update

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+require("dotenv").config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 
-mongoose.connect('mongodb+srv://masoncapps:Mc1013986@ratingprofilesapi.qerbcar.mongodb.net/Rating-Profiles-API?retryWrites=true&w=majority').then(() => {
+mongoose.connect(`mongodb+srv://${process.env.ADMIN_ID}@ratingprofilesapi.qerbcar.mongodb.net/Rating-Profiles-API?retryWrites=true&w=majority`).then(() => {
   console.log('connected to MongoDB')
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
