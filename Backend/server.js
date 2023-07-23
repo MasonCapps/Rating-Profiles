ac7c7de9f6ce9d25731e106c1fb987aa45364008
Mason Capps <masoncapps42119@gmail.com>
2023-07-23 11:36:12 -0500

    Adds functional get single user route by passing ID parameter

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,16 @@ app.get('/users', async(req, res) => {
   }
 })
 
+app.get('/users/:id', async(req, res) => {
+  try {
+    const {id} = req.params
+    const user = await User.findById(id)
+    res.status(200).json(user)
+  } catch(error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
 app.post('/user', async(req, res) => {
   console.log("posting")
   try {
