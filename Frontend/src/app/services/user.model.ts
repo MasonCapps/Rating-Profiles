ea4c1a9988a984005b7b6104203426f9fd72a7d2
Mason Capps <masoncapps42119@gmail.com>
2023-08-04 19:17:00 -0500

    Initializes the user attributes in the User class

--- Frontend/src/app/services/user.model.ts
+++ Frontend/src/app/services/user.model.ts
@@ -1,2 +1,15 @@
 export class User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  strength: number;
+  speed: number;
+  rizz: number;
+  magic: number;
+  stealth: number;
+  health: number;
+  kd: number;
+  ratedBy: number;
+  rated: number
 }
