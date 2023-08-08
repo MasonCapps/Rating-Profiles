049fa8e003c5dc38f7a3b511f284ca10274c6a9c
Mason Capps <masoncapps42119@gmail.com>
2023-08-07 19:39:02 -0500

    Adds more required imports

--- Frontend/src/app/components/user/user.component.ts
+++ Frontend/src/app/components/user/user.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../../services/user.service'
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [UserService]
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
